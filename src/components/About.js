517e2335156ded81d7f77b90a30ec08a79afa28c
robbyknee85 <89692669+robbyknee85@users.noreply.github.com>
2022-06-25 14:20:51 +0500

    notes hard code data

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React,{useContext} from 'react'
+import noteContext from '../contex/notes/noteContext';
+
+ const About = () => {
+  
+    
+  return (
+    <div>this is about</div>
+  )
+}
+ export default About;
